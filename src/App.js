b459c21488aee36268732478100862f934aadcf9
bui trung hieu <trunghieuu2k4@gmail.com>
2025-08-20 00:24:02 +0700

    First message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+
+const App = () => {
+
+  return (
+    <div className="app-container">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
